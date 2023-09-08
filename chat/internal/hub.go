4184210aa530e41bb7924f668a00c4f7152268b1
fresh888888 <fresh888888@foxmail.com>
2023-09-08 13:58:33 +0800

    chat

---
+++ chat/internal/hub.go
@@ -0,0 +1,43 @@
+package internal
+
+// Hub maintains the set of active clients and broadcasts messages to the clients
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+	// Inbound messages from the clients
+	broadcast chan []byte
+	// Register requests from the clients
+	register chan *Client
+	// unregister request from the clients
+	unregister chan *Client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -36,6 +36,7 @@ require (
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
